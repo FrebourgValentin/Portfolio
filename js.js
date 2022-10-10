5f45ea3f320659cbd6d0922b93470d9149e59062
Valentin <114942401+FrebourgValentin@users.noreply.github.com>
2022-10-10 16:29:22 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -13,69 +13,19 @@ var tagCloud = TagCloud('.sphere', competences, {
 /*====================== SECTION REALISATIONS ======================== */
 
 const slider = document.querySelector(".slider")
-const trail = document.querySelector(".trail").querySelectorAll("div")
-
-let value = 0
-let trailValue = 0
-let interval = 10000
-
-// const slide = (condition) => {
-//   clearInterval(start)
-//   condition === "increase" ? initiateINC() : initiateDEC()
-//   move(value, trailValue)
-//   animate()
-//   start = setInterval(() => slide("increase"), interval);
-// }
-
-// const initiateINC = () => {
-//   trail.forEach(cur => cur.classList.remove("active"))
-//   value === 80 ? value = 0 : value += 20
-//   trailUpdate()
-// }
-
-// const initiateDEC = () => {
-//   trail.forEach(cur => cur.classList.remove("active"))
-//   value === 0 ? value = 80 : value -= 20
-//   trailUpdate()
-// }
-
-const move = (S, T) => {
-  slider.style.transform = `translateX(-${S}%)`
-  trail[T].classList.add("active")
-}
+const trail = document.querySelectorAll(".trail div")
 
 const tl = gsap.timeline({ defaults: { duration: 0.6, ease: "power2.inOut" } })
 tl.from("#realisations .bg", { x: "-100%", opacity: 0 })
   .from("#realisations p", { opacity: 0 }, "-=0.3")
   .from("#realisations h2", { opacity: 0, y: "30px" }, "-=0.3")
 
-const animate = () => tl.restart()
-
-const trailUpdate = () => {
-  if (value === 0) {
-    trailValue = 0
-  } else if (value === 20) {
-    trailValue = 1
-  } else if (value === 40) {
-    trailValue = 2
-  } else if (value === 60) {
-    trailValue = 3
-  } else {
-    trailValue = 4
-  }
-}
-
-let start = setInterval(() => slide("increase"), interval)
-
-// document.querySelectorAll("img").forEach(cur => {
-//   cur.addEventListener("click", () => cur.classList.contains("fleche_droite") ? slide("increase") : slide("decrease"))
-// })
-
 const clickCheck = (e) => {
-  clearInterval(start)
   trail.forEach(cur => cur.classList.remove("active"))
   const check = e.target
   check.classList.add("active")
+
+  let value = 0
 
   if (check.classList.contains("box1")) {
     value = 0
@@ -88,32 +38,9 @@ const clickCheck = (e) => {
   } else {
     value = 80
   }
-  trailUpdate()
-  move(value, trailValue)
-  animate()
-  start = setInterval(() => slide("increase"), interval)
+
+  slider.style.transform = `translateX(-${value}%)`
+  tl.restart()
 }
 
 trail.forEach(cur => cur.addEventListener("click", (ev) => clickCheck(ev)))
-
-// const touchSlide = (() => {
-//   let start, move, change, sliderWidth
-
-//   slider.addEventListener("touchstart", (e) => {
-//     start = e.touches[0].clientX
-//     sliderWidth = slider.clientWidth / trail.length
-//   })
-
-//   slider.addEventListener("touchmove", (e) => {
-//     e.preventDefault()
-//     move = e.touches[0].clientX
-//     change = start - move
-//   })
-
-//   const mobile = (e) => {
-//     change > (sliderWidth / 4) ? slide("increase") : null;
-//     (change * -1) > (sliderWidth / 4) ? slide("decrease") : null;
-//     [start, move, change, sliderWidth] = [0, 0, 0, 0]
-//   }
-//   slider.addEventListener("touchend", mobile)
-// })()
